88c01a8b2d64c5611f801e68437b68ed58385652
latt_chanon <chanonlatt@gmail.com>
2022-03-15 15:11:26 +0700

    Initial Commit

---
+++ Vertical CollectionView inslide TableViewCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vertical CollectionView inslide TableViewCell
+//
+//  Created by Chanon Latt on 3/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
